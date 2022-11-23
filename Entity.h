4d474db48b53fbfd3fcdfe4e99b5e27b519aa64f
jling8208 <118538329+jling8208@users.noreply.github.com>
2022-11-23 16:30:31 +0800

    Update Entity.h

--- Entity.h
+++ Entity.h
@@ -1,3 +1,40 @@
 #pragma once
 
+#include "Entity.h"
+#include "Position.h"
 #include <SDL.h>
+
+enum direction { DEFAULT, UP, DOWN, LEFT, RIGHT };
+
+class Entity
+{
+public:
+	Entity(position p_pos, SDL_Texture* p_tex);
+	position& getPos()
+	{
+		return pos;
+	}
+	direction getDir()
+	{
+		return dir;
+	}
+	float getAngle()
+	{
+		return angle;
+	}
+	Vector2f getScale()
+	{
+		return scale;
+	}
+	void setPos(float x, float y);
+	void setDir(direction dir);
+	SDL_Texture* getTex();
+	void update();
+	void setCurrentFrame(int x, int y, int w, int h, SDL_Texture* p_tex);
+	SDL_Rect getCurrentFrame();
+private:
+	position pos;
+	direction dir = DEFAULT;
+	SDL_Rect currentFrame;
+	SDL_Texture* tex;
+};
