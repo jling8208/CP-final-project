763ebd5303acef070d6531d423551b4832533667
jling8208 <118538329+jling8208@users.noreply.github.com>
2022-11-23 16:12:28 +0800

    Update Position.h

--- Position.h
+++ Position.h
@@ -4,17 +4,12 @@
 
 struct position
 {
-	position()
-		:x(0.0f), y(0.0f)
-	{}
-
-	position(float p_x, float p_y)
-		:x(p_x), y(p_y)
-	{}
+	position(): x(0.0f), y(0.0f) {}
+	position(float p_x, float p_y): x(p_x), y(p_y) {}
 
 	void print()
 	{
-		std::cout << x << ", " << y << std::endl;
+		std::cout << "Position at (" << x << ", " << y << ")" << std::endl;
 	}
 
 	float x, y;
