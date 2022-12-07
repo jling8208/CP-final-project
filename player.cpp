bec7fee91fb0dc57b9db73a683e11460a3f4026b
jling8208 <118538329+jling8208@users.noreply.github.com>
2022-12-07 21:51:12 +0800

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -2,7 +2,7 @@
 #include "Entity.h"
 #include "Math.h"
 #include "Tile.h"
-#include "Hole.h"
+#include "Bullet.h"
 
 #include <vector>
 #include <windows.h>
@@ -11,13 +11,12 @@
 #include <SDL_ttf.h>
 #include <SDL_mixer.h>
 
-Player::Player(Vector2f p_pos, SDL_Texture* p_tex, SDL_Texture* p_pointTex, SDL_Texture* p_powerMTexFG, SDL_Texture* p_powerMTexBG)
+Player::Player(Vector2f p_pos, SDL_Texture* p_tex, SDL_Texture* p_pointTex, SDL_Texture* p_healthMTexFG, SDL_Texture* p_healthMTexBG)
     :Entity(p_pos, p_tex)
 {
-    points.push_back(Entity(Vector2f(-64, -64), p_pointTex));
-    setScale(4, 4);
-    //powerBar.push_back(Entity(Vector2f(-64, -64), p_powerMTexBG));
-    //powerBar.push_back(Entity(Vector2f(-64, -64), p_powerMTexFG));
+    setScale(1.5, 1.5);
+    healthBar.push_back(Entity(Vector2f(-64, -64), p_healthMTexBG));
+    healthBar.push_back(Entity(Vector2f(-64, -64), p_healthMTexFG));
 }
 
 void Player::setVelocity(float x, float y)
@@ -26,10 +25,9 @@ void Player::setVelocity(float x, float y)
     velocity.y = y;
 }
 
-void Player::setLaunchedVelocity(float x, float y)
+void Player::setV_factor(float f)
 {
-    launchedVelocity.x = x;
-    launchedVelocity.y = y;
+    v_factor = f;
 }
 
 void Player::setInitialMousePos(float x, float y)
@@ -38,75 +36,80 @@ void Player::setInitialMousePos(float x, float y)
     initialMousePos.y = y;
 }
 
-void Player::setDead(bool p_die)
+void Player::addHealth(float healthVal)
 {
-    dead = p_die;
+    health += healthVal;
+
+    if (health < 0)
+    {
+        health = 0;
+    }
+    if (health > 100) 
+    { 
+        health = 100; 
+    }
 }
 
-void Player::update(double deltaTime, bool keyDown, std::vector<Tile> tiles, std::vector<Hole> holes, Mix_Chunk* chargeSfx, Mix_Chunk* swingSfx, Mix_Chunk* holeSfx)
+double Player::getAim()
 {
-    if (dead)
-    {
-        if (getPos().x < target.x)
-        {
-            setPos(getPos().x += 0.1 * deltaTime, getPos().y);
-        }
-        else if (getPos().x > target.x)
-        {
-            setPos(getPos().x -= 0.1 * deltaTime, getPos().y);
-        }
-        if (getPos().y < target.y)
-        {
-            setPos(getPos().x, getPos().y += 0.1 * deltaTime);
-        }
-        else if (getPos().y > target.y)
-        {
-            setPos(getPos().x, getPos().y -= 0.1 * deltaTime);
-        }
-        setScale(getScale().x - 0.001 * deltaTime, getScale().y - 0.001 * deltaTime);
-        return;
-    }
+    int mouseX = 0;
+    int mouseY = 0;
+    SDL_GetMouseState(&mouseX, &mouseY);
+    aim = SDL_atan2(mouseY - getPos().y, mouseX - getPos().x) * (180 / 3.1415) + 90;
+    return aim;
+}
 
-    for (Hole h : holes)
-    {
-        if (getPos().x + 4 > h.getPos().x && getPos().x + 16 < h.getPos().x + 20 && getPos().y + 4 > h.getPos().y && getPos().y + 16 < h.getPos().y + 20)
-        {
-            Mix_PlayChannel(-1, holeSfx, 0);
-            setDead(true);
-            target.x = h.getPos().x;
-            target.y = h.getPos().y + 3;
-        }
-    }
+void Player::update(double deltaTime, bool keyDown, std::vector<Tile> tiles)
+{
+    healthBar.at(0).setPos(getPos().x + 32 + 8, getPos().y + 32);
+    healthBar.at(1).setPos(getPos().x + 32 + 8 + 4, getPos().y + 32 + 4);
+    healthBar.at(1).setScale(health / 100, 1);
 
     if (keyDown)
     {
         switch (Entity::getDir())
         {
         case (UP):
-            if (getPos().y >= 0)
+            if (getPos().y > 0)
             {
-                setVelocity(0, -init_v);
-            }           
+                setVelocity(0, -v_init);
+            }
+            else
+            {
+                setVelocity(0, 0);
+            }
             break;
 
         case (DOWN):
-            if (getPos().y + getCurrentFrame().h <= 480)
+            if (getPos().y + getCurrentFrame().h * getScale().y < 480)
             {
-                setVelocity(0, init_v);
-            }          
+                setVelocity(0, v_init);
+            }
+            else
+            {
+                setVelocity(0, 0);
+            }
             break;
 
         case (LEFT):
-            if (getPos().x >= 0)
+            if (getPos().x > 0)
             {
-                setVelocity(-init_v, 0);
-            }           
+                setVelocity(-v_init, 0);
+            }
+            else
+            {
+                setVelocity(0, 0);
+            }
             break;
 
         case (RIGHT):
-            if (getPos().x + getCurrentFrame().w <= 640)
+            if (getPos().x + getCurrentFrame().w * getScale().x < 640)
             {
-                setVelocity(init_v, 0);
+                setVelocity(v_init, 0);
+            }
+            else
+            {
+                setVelocity(0, 0);
             }
             break;
         }
@@ -114,23 +117,23 @@ void Player::update(double deltaTime, bool keyDown, std::vector<Tile> tiles, std
     else
     {
         setVelocity(0, 0);
-    }  
+    }
 
-    for (Tile& t : tiles)
+    for (Obstacle& o : obstacles)
     {
-        float newX = getPos().x + getVelocity().x;
+        float newX = getPos().x + getVelocity().x * deltaTime;
         float newY = getPos().y;
-        if (newX + 10 > t.getPos().x && newX < t.getPos().x + t.getCurrentFrame().w && newY + 37 > t.getPos().y && newY < t.getPos().y + t.getCurrentFrame().h - 3)
+        if (newX + getCurrentFrame().w * getScale().x > o.getPos().x && newX < o.getPos().x + o.getCurrentFrame().w && newY + getCurrentFrame().h * getScale().y > o.getPos().y && newY < o.getPos().y + o.getCurrentFrame().h - 3)
         {
             setVelocity(0, getVelocity().y);
         }
 
         newX = getPos().x;
-        newY = getPos().y + getVelocity().y;
-        if (newX + 10 > t.getPos().x && newX < t.getPos().x + t.getCurrentFrame().w && newY + 37 > t.getPos().y && newY < t.getPos().y + t.getCurrentFrame().h - 3)
+        newY = getPos().y + getVelocity().y * deltaTime;
+        if (newX + getCurrentFrame().w * getScale().x > o.getPos().x && newX < o.getPos().x + o.getCurrentFrame().w && newY + getCurrentFrame().h * getScale().y > o.getPos().y && newY < o.getPos().y + o.getCurrentFrame().h - 3)
         {
             setVelocity(getVelocity().x, 0);
         }
     }
-    setPos(getPos().x + getVelocity().x, getPos().y + getVelocity().y);
+    setPos(getPos().x + getVelocity().x * deltaTime, getPos().y + getVelocity().y * deltaTime);
 }
