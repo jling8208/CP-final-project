830c15ce88215dd7f888cc1e4cdd50b9723e7e57
jling8208 <118538329+jling8208@users.noreply.github.com>
2022-12-10 15:41:59 +0800

    Update Bullet.h

--- Bullet.h
+++ Bullet.h
@@ -1,3 +1,37 @@
 #pragma once
-
 #include <SDL.h>
+#include <SDL_image.h>
+#include <vector>
+#include "Hole.h"
+#include "Obstacle.h"
+
+#include "Entity.h"
+#include "Math.h"
+
+class Bullet : public Entity
+{
+public:
+    Bullet(Vector2f p_pos, double angle, SDL_Texture* p_tex);
+    Vector2f& getVelocity()
+    {
+        return velocity;
+    }
+    bool isHit()
+    {
+        return hit;
+    }
+    void setHit(bool p_hit);
+    void setVelocity(float x, float y);
+    void setLaunchedVelocity(float x, float y);
+    void update(double deltaTime, bool mouseDown, bool mousePressed, std::vector<Obstacle> obstacles);
+private:
+    Vector2f velocity;
+    Vector2f target;
+    Vector2f launchedVelocity;
+    float velocity1D = 0.3;
+    Vector2f initialMousePos;
+    int dirX = 1;
+    int dirY = 1;
+    bool hit = false;
+    int hit_times = 0;
+};
