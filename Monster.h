8b892db0a2223e0c53f22c84a3100cfdbfbd176c
Jikoa <104753628+Jikoa@users.noreply.github.com>
2022-12-14 16:31:18 +0800

    Update Monster.h
    copy from player.h

--- Monster.h
+++ Monster.h
@@ -1,3 +1,42 @@
 #pragma once
 
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <vector>
+#include "Bullet.h"
+#include "Tile.h"
+
+#include "Entity.h"
+#include "Math.h"
+
+class Monster : public Entity{
+public:
+    Monster(Vector2f p_pos, SDL_Texture* p_tex, SDL_Texture* p_pointTex, SDL_Texture* p_powerMTexFG, SDL_Texture* p_powerMTexBG);
+    Vector2f& getVelocity()
+    {
+        return velocity;
+    }
+    bool isDead()
+    {
+        return health == 0;
+    }
+    std::vector<Entity> getHealthBar()
+    {
+        return healthBar;
+    }
+    void addHealth(float healthVal);
+    void setInitialMousePos(float x, float y);
+    void setVelocity(float x, float y);
+    void setV_factor(float f);
+    double getAim();
+    void update(double deltaTime, bool keyDown, std::vector<Obstacle> obstacles);
+private:
+    Vector2f velocity = Vector2f(0, 0);
+    double aim;
+    float v_init = 0.3;
+    float v_factor = 1;
+    std::vector<Entity> healthBar;
+    float health = 100;
+    Vector2f initialMousePos;
+};
